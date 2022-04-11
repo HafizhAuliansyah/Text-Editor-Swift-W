da24711f49036e5a79635c924f9c1543f63c8720
HafizhAuliansyah <hafizh.auliansyah@gmail.com>
2022-04-11 11:22:06 +0700

    Menambahkan komentar

--- cursor.cpp
+++ cursor.cpp
@@ -8,13 +8,17 @@ void moveCursor(int key){
 	}
 }
 void getCursorPosition(int *row,int *column){
+    // Mengambil konfigurasi console out
     HANDLE ConsoleOut = getConsoleOut();
+    // Mengambil konfigurasi screen buffer info
     if(!GetConsoleScreenBufferInfo(ConsoleOut, &infoScreenBuffer)){
         MessageBox(NULL, TEXT("Error Pengambilan Info Screen Buffer"), TEXT("ERROR"), MB_OK);
     }
+    // Memasukkan koordinat cursor ke struct cursor handler
     cursor.x = infoScreenBuffer.dwCursorPosition.X;
     cursor.y = infoScreenBuffer.dwCursorPosition.Y;
-    *row = cursor.Y;
+    // Mengambalikan nilai
+    *row = cursor.y;
     *column = cursor.x;
 }
 
--- main.cpp
+++ main.cpp
@@ -5,6 +5,7 @@
 
 int main () {
 	rawModeOn();
+
 	while(1){
 		keyProcess();
 	}
